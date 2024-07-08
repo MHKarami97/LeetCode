4604cef63ec990c20ace9f4faf2d4f127562ca3e
mhkarami97 <mhkarami97@gmail.com>
2024-07-08 14:19:08 +0330

    feat: add p242

--- Problems/P242.cs
+++ Problems/P242.cs
@@ -1,10 +1,71 @@
-﻿namespace LeetCode.Problems;
+﻿using System.Collections;
 
+namespace LeetCode.Problems;
+
+/*
+ * Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+
+
+Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+ */
 public class P242
 {
     public bool IsAnagram(string s, string t)
     {
+        if (s.Length != t.Length)
+            return false;
 
-        return false;
+        var hashtable = new Hashtable(s.Length);
+        var hashtable1 = new Hashtable(s.Length);
+
+        for (var item = 0; item < s.Length; item++)
+        {
+            if (!hashtable.ContainsKey(s[item]))
+            {
+                hashtable.Add(s[item], 1);
+            }
+            else
+            {
+                hashtable[s[item]] = (int)hashtable[s[item]] + 1;
+            }
+
+            if (!hashtable1.ContainsKey(t[item]))
+            {
+                hashtable1.Add(t[item], 1);
+            }
+            else
+            {
+                hashtable1[t[item]] = (int)hashtable1[t[item]] + 1;
+            }
+        }
+
+        foreach (DictionaryEntry item in hashtable)
+        {
+            if (!item.Value.Equals(hashtable1[item.Key]))
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using LeetCode.Problems;
+
+Console.WriteLine("Start");
+
+const int ProblemId = 242;
+
+switch (ProblemId)
+{
+    case 242:
+        var p242 = new P242().IsAnagram("anagram", "nagaram");
+        break;
+
+    default:
+        Console.WriteLine("Not valid problem id");
+}
