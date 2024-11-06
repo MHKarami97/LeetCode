db68209d7cae00e7b2ae5a8574b66ff4e5a53133
mhkarami97 <mhkarami97@gmail.com>
2024-11-06 16:30:52 +0330

    feat: add benchmark

--- Program.cs
+++ Program.cs
@@ -1,19 +1,36 @@
-﻿using LeetCode.Problems;
+﻿using BenchmarkDotNet.Running;
+using LeetCode;
+using LeetCode.Problems;
 
 Console.WriteLine("Start");
 
-const int ProblemId = 53;
-
-switch (ProblemId)
+try
 {
-    case 242:
-        var p242 = new P242().IsAnagram("anagram", "nagaram");
-        break;
+    const int ProblemId = 53;
+    const bool NeedBenchmark = false;
 
-    case 53:
-        var p53 = new P53().MaxSubArray(new[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 });
-        break;
 
-    default:
-        Console.WriteLine("Not valid problem id");
+    switch (ProblemId)
+    {
+        case 242:
+            var p242 = new P242().IsAnagram("anagram", "nagaram");
+            break;
+
+        case 53:
+            var p53 = new P53().MaxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]);
+            break;
+
+        default:
+            Console.WriteLine("Not valid problem id");
+            break;
+    }
+
+    if (NeedBenchmark)
+    {
+        BenchmarkRunner.Run<BenchmarkCode>();
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
 }
